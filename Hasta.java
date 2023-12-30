ac5c1dda90ea56308655cd615cafe5dcbe43d24d
Toprak Necat Gök <114919715+T-Necat@users.noreply.github.com>
2023-12-30 13:41:19 +0300

    Hasta Ekleme /Silme Fonksiyonları eklendi

--- Hasta.java
+++ Hasta.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
+
 
 public class Hasta extends AbsractKisi{
+
+    static Scanner tarayici = new Scanner(System.in);
     public static ArrayList<Hasta> hastaListesi=new ArrayList<Hasta>();
 
     public Hasta(String isim,String soyisim,String TC,String dogumTarihi,int id,int telefonNo,String dogumYeri) {
@@ -12,5 +17,64 @@ public class Hasta extends AbsractKisi{
         this.telefonNo=telefonNo;
         this.dogumYeri=dogumYeri;
         hastaListesi.add(this);
+    }
+
+
+    // Hasta silme fonksiyonu
+    public static void hastaSil(){
+
+        System.out.println("Silmek istenen hastanın İD'sini giriniz ");
+        int silinenHastaId = tarayici.nextInt();
+
+        //Hastanın olup olmama furmuna göre flag ile kontrol edilir.
+        boolean flag= false;
+
+        // id ile tüm hastalar araında arama yaparak istenen hasta silinir.
+        for (int i = 0; i < Hasta.hastaListesi.size(); i++) {
+            if (Hasta.hastaListesi.get(i).id==silinenHastaId){
+                Hasta.hastaListesi.remove(Hasta.hastaListesi.get(i));
+                System.out.println(
+                        Hasta.hastaListesi.get(i).isim + " isimli ve " +
+                        Hasta.hastaListesi.get(i).id + " İd numaralı Hasta silindi. ");
+                flag = true;
+            }
+        }
+        if (!flag){System.out.println("Aradığınız kullanıcı bulunmamaktadır.");}
+
+    }
+
+
+    // Hasta Ekleme Fonksiyonu
+
+    public static void hastaEkle() {
+
+        //Eklenmek istenen hastanın gerkli bilgileri girilir
+        System.out.println("Gerekli hasta bilgilerini giriniz : ");
+        System.out.println(" Hasta ismi : ");
+        String isim = tarayici.nextLine();
+
+        System.out.println(" Hasta soyismi : ");
+        String soyisim = tarayici.nextLine();
+
+        System.out.println(" Hasta Tc : ");
+        String tc = tarayici.nextLine();
+
+        System.out.println(" Hasta doğum tarihi : ");
+        String dogumTarihi = tarayici.nextLine();
+
+        System.out.println(" Hasta telefon numarası : ");
+        int telefonNumarasi = tarayici.nextInt();
+
+        System.out.println(" Hasta doğum yeri : ");
+        String dogumYeri = tarayici.nextLine();
+
+        //id numarası liste boyutuna göre belirlenir
+        int id = Hasta.hastaListesi.size();
+        id++;
+
+        // Yapıcı metot listeye ekler
+        Hasta h = new Hasta(isim,soyisim,tc,dogumTarihi,id,telefonNumarasi,dogumYeri);
+
+
     }
 }
